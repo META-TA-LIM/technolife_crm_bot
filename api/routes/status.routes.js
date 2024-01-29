4527c8aca673bf20e36580c1aa67cb076f36437c
Ziyodilla <ziyodillazokirkhujayev@gmail.com>
2024-01-29 15:32:52 +0500

    fix: fixed bot bugs

--- api/routes/status.routes.js
+++ api/routes/status.routes.js
@@ -5,6 +5,7 @@ const {
   statusGet,
   statusGetOne,
   statusPost,
+  statusHide,
 } = require("../controller/status.controller");
 const isAdmin = require("../middlewares/isAdmin.middleware");
 const isAuth = require("../middlewares/isAuth.middleware");
@@ -15,6 +16,7 @@ router.post("/status/create", isAuth, isAdmin, statusPost);
 router.get("/status/list", isAuth, statusGet);
 router.get("/status/:id", isAuth, statusGetOne);
 router.put("/status/update/:id", isAuth, isAdmin, statusEdit);
+router.put("/status/hide/:id", isAuth, isAdmin, statusHide);
 router.delete("/status/delete/:id", isAuth, isAdmin, statusDelete);
 
 module.exports = router;
